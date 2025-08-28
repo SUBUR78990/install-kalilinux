57a97bed84ce65c5fc4dd615d2e2e6b776e5d5e0
AbangRombeng <80770646+SUBUR78990@users.noreply.github.com>
2025-08-29 03:27:57 +0700

    Create Kali.sh
    Signed-off-by: AbangRombeng <80770646+SUBUR78990@users.noreply.github.com>

---
+++ Kali.sh
@@ -0,0 +1,11 @@
+ termux-setup-storage
+
+ apt upgrade
+
+ apt install wget
+
+ wget -O install-nethunter-termux https://offs.ec/2MceZWr
+
+ chmod +x install-nethunter-termux
+
+ ./install-nethunter-termux
